5d3e5d22ec00c4cebf8c19acd87aa28fe54f977a
Michael Ford <8105141+mikeyford@users.noreply.github.com>
2017-11-26 00:22:52 +0000

    maps

--- src/exploration.R
+++ src/exploration.R
@@ -8,6 +8,10 @@ data <- read.csv("data/london-borough-profiles.csv")
 data <- subset(data, (Inner_Outer_London=="Inner_London")|(Inner_Outer_London=="Outer_London"))
 data <- na.mean(data, option = "mean")  #impute NA values with mean
 
+data <- merge(data, ascData, by.x = "Code", by.y = "ONS_Code")
+
+data$asc_per_pop <- round(data$asc_total/data$GLA_Population_Estimate_2015,1)
+data$mental_health_spend_per_pop <- round((data$X65_and_over_Mental_Health_Support+data$X18_to_64_Mental_Health_Support)/data$GLA_Population_Estimate_2015,1)
 
 dropList = c("GLA_Population_Estimate_2015",
              "GLA_Household_Estimate_2015",
@@ -29,11 +33,29 @@ subData <- data[, !colnames(data) %in% dropList]
 
 C <- round(cor(select_if(subData, is.numeric)), 2)
 cTab <- as.data.frame(as.table(C))
-cLarge <- subset(cTab, (abs(Freq) > 0.75)&(Freq) != 1)
+cLarge <- subset(cTab, (abs(Freq) > 0.70)&(Freq) != 1)
 cLarge <- unique(as.data.frame(t(apply(cLarge, 1, sort))))
 
 
 boroughs <- readOGR(dsn = "data/stats-boundaries-london/ESRI/", layer = "London_Borough_Excluding_MHW")
+boroughs <- merge(boroughs, data, by.x = "GSS_CODE", by.y = "Code" )
+
+ascData <- read.csv("data/boroughs_ASC.csv")
+boroughs <- merge(boroughs, ascData, by.x = "GSS_CODE", by.y = "ONS_Code")
 
 
+boroughs$asc_per_pop <- round(boroughs$asc_total/boroughs$GLA_Population_Estimate_2015,1)
+boroughs$mental_health_spend_per_pop <- round((boroughs$X65_and_over_Mental_Health_Support+boroughs$X18_to_64_Mental_Health_Support)/boroughs$GLA_Population_Estimate_2015,1)
+
+selection <- boroughs$mental_health_spend_per_pop>mean(boroughs$mental_health_spend_per_pop)
+plot(boroughs, col = "lightgrey")
+plot(boroughs[selection, ], col = "blue", add = TRUE)
+spplot(boroughs[-33,], "Anxiety_score_201114_out_of_10", sub = "Anxiety score (self-rated)", col = "transparent")
+
+
+spplot(boroughs[-33,], "asc_per_pop", sub = "Total adult social care spending per capita", col = "transparent")
+spplot(boroughs[-33,], "mental_health_spend_per_pop", sub = "Mental health spending per capita", col = "transparent")
+
+
+#look into how where over 65 ASC spending does not increase with proportion of over 65s
 
