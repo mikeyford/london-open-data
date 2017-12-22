9eb31703f4cb781e66f6c923d9fe1f861fa9003f
Michael Ford <8105141+mikeyford@users.noreply.github.com>
2017-12-22 22:41:09 +0000

    finalised ward analysis

--- src/wards.R
+++ src/wards.R
@@ -20,7 +20,61 @@ wards$pop_density <- wards$population/wards$HECTARES
 spplot(wards, "bad_health_rate", sub = "Rate of bad health (self-reported)", col = "transparent")
 spplot(wards, "Aged_Over65", sub = "Over 65 rate", col = "transparent")
 
-names(wards@data) <- abbreviate(names(wards@data), minlength=8) 
+#names(wards@data) <- abbreviate(names(wards@data), minlength=8) 
 
-correlation <- round(cor(select_if(na.omit(wards@data), is.numeric)), 2)
-corrplot(correlation, method = "square")
+data <- wards@data
+
+keepList <- c("pop_density",
+              "Aged_0_15",
+              "Aged_16_64",
+              "Aged_Over65",
+              "Mean_NOx",
+              "bad_very_bad_health",
+              "Life_Expectancy_200913",
+              "Childhood_Obesity_2013",
+              "Incapacity_Benefit_rate__2013",
+              "Unemployment_rate_2013",
+              "Crime_rate__2013",
+              "Deliberate_Fires__2013",
+              "Subjective_wellbeing_average_score_2013",
+              "Public_Transport_Accessibility__2013")
+subData <- data[, colnames(data) %in% keepList]
+C <- round(cor(na.omit(subData)), 3)
+
+rownames(C)<- c("Mean NOx",
+                "Bad or V. Bad Health",
+                "Pop. Under 16 %",
+                "Pop. Working Age %",
+                "Pop. Over 65 %",
+                "Life Expectancy",
+                "Child Obesity Rate",
+                "Disabled Benefit Rate",
+                "Unemployment Rate",
+                "Crime Rate",
+                "Deliberate Fire Rate",
+                "Public Transit Access",
+                "Wellbeing Score",
+                "Pop. Density")
+
+colnames(C) <- c("NOx",
+                 "BVBH",
+                 "PU16",
+                 "PWA",
+                 "PO65",
+                 "LE", 
+                 "COR",
+                 "DBR",
+                 "UR",
+                 "CR",
+                 "DFR",
+                 "PTA",
+                 "WS",
+                 "PD")
+
+
+
+
+
+#C <- round(cor(select_if(na.omit(wards@data), is.numeric)), 2)
+
+corrplot(C, method = "color",  tl.col = "black")
